23c1622d6d52dab09d2d5b692bdf8c90e465dfdd
Alex Sweet <alexsweet31@gmail.com>
2018-12-24 17:17:09 -0500

    update

--- parallax.js
+++ parallax.js
@@ -1,14 +1,22 @@
+const parallaxSection = document.querySelector('section.parallax-1')
 
 
 
 
-
-
-
-
-
-
-
+const parallax1 = function(){
+        
+        const topViewport = window.pageYOffset
+        const midViewport = topViewport + (window.innerHeight / 2)
+        const parallaxSectionTop = parallaxSection.offsetTop
+        const parallaxSectionMid = parallaxSectionTop + (parallaxSection.offsetHeight / 2)
+        
+        const distanceToSection = midViewport - parallaxSectionMid
+        
+        const parallaxLetters = document.querySelector('section.parallax-1 img')
+        
+        
+        
+}
 
 
 
--- type-tester.js
+++ type-tester.js
@@ -1,5 +1,16 @@
+const typeTesterTag = document.querySelector('section.type-tester')
+
 const typeSizeTag = document.querySelector(`input[name="type-size"]`)
+const leadingTag = document.querySelector(`input[name="leading"]`)
+const trackingTag = document.querySelector(`input[name="tracking"]`)
 const styleTag = document.querySelector(`select[name="typeface"]`)
+
+const typeSizeOutput = document.querySelector('.typesize-output')
+const leadingOutput = document.querySelector('.leading-output')
+const trackingOutput = document.querySelector('.tracking-output')
+
+const circleTag = document.querySelectorAll('div.circle')
+
 const outputTag = document.querySelector('div.output')
 
 
@@ -9,15 +20,45 @@ const outputTag = document.querySelector('div.output')
 typeSizeTag.addEventListener('input', function(){
     
     outputTag.style.fontSize = this.value + 'px'
+    typeSizeOutput.innerHTML = this.value + 'px'
     
 })
+
+leadingTag.addEventListener('input', function(){
+    
+    outputTag.style.lineHeight = this.value
+    leadingOutput.innerHTML = this.value
+    
+    
+})
+trackingTag.addEventListener('input', function(){
+    
+    outputTag.style.letterSpacing = this.value + 'em'
+    trackingOutput.innerHTML = this.value + 'em'
+})
+
+
+
 
 styleTag.addEventListener('input', function(){
     
     outputTag.style.fontFamily = this.value
     
+    
 })
 
-
+circleTag.forEach(circle =>{
+ 
+    circle.addEventListener('click', function(){
+        const bgColor = this.getAttribute('data-color')
+        const typeColor = this.getAttribute('data-typecolor')
+        
+        typeTesterTag.style.backgroundColor= bgColor
+        typeTesterTag.style.color= typeColor
+        outputTag.style.color= typeColor
+   
+    })
+   
+})
 
 
