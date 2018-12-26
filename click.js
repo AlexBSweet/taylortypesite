95a62d96cddcfc26f2eaa3fea5aeee371a5b17bb
Alex Sweet <alexsweet31@gmail.com>
2018-12-26 02:17:02 -0500

    added click letters

--- click.js
+++ click.js
@@ -3,9 +3,33 @@ const clearTag = document.querySelector('div.clear')
 
 let imageNumber = 0
 const clickImages = [
-    'images/click-04.svg',
-    'images/click-04.svg',
-    'images/click-04.svg'
+     'images/click-06.svg',
+    'images/click-06.svg',
+    'images/click-07.svg',
+    'images/click-08.svg',
+    'images/click-09.svg',
+    'images/click-10.svg',
+    'images/click-11.svg',
+    'images/click-12.svg',
+    'images/click-13.svg',
+    'images/click-14.svg',
+    'images/click-15.svg',
+    'images/click-16.svg',
+    'images/click-17.svg',
+    'images/click-18.svg',
+    'images/click-19.svg',
+    'images/click-31.svg',
+    'images/click-30.svg',
+    'images/click-29.svg',
+    'images/click-28.svg',
+    'images/click-27.svg',
+    'images/click-26.svg',
+    'images/click-25.svg',
+    'images/click-24.svg',
+    'images/click-23.svg',
+    'images/click-22.svg',
+    'images/click-21.svg',
+    'images/click-20.svg'
 ]
 
 
@@ -20,6 +44,9 @@ const addStamp = function(x, y){
     
     img.setAttribute('src', clickImages[imageNumber])
     clickTag.appendChild(img)
+    
+
+    
 }
 
 
@@ -30,6 +57,7 @@ document.addEventListener('click', function(event){
     
     clearTag.classList.add('clear-open')
     
+    
 })
 
 clearTag.addEventListener('click', function(){
@@ -37,5 +65,11 @@ clearTag.addEventListener('click', function(){
     clickTag.innerHTML = ''
     clearTag.classList.remove('clear-open')
 })
+
+
+
+
+
+
 
 
