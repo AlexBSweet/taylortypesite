121684cb38122ffe115d4dcb82f57a073c217947
Alex Sweet <alexsweet31@gmail.com>
2018-12-26 23:39:47 -0500

    added parallax, finalized structure

--- parallax.js
+++ parallax.js
@@ -1,26 +1,23 @@
-const parallaxSection = document.querySelector('section.parallax-1')
+const sectionsTag = document.querySelector('section.parallax')
 
+document.addEventListener('scroll', function(){
+  const topViewport = window.pageYOffset
+  const midViewport = topViewport + (window.innerHeight / 2)
+  
 
-
-
-const parallax1 = function(){
-        
-        const topViewport = window.pageYOffset
-        const midViewport = topViewport + (window.innerHeight / 2)
-        const parallaxSectionTop = parallaxSection.offsetTop
-        const parallaxSectionMid = parallaxSectionTop + (parallaxSection.offsetHeight / 2)
-        
-        const distanceToSection = midViewport - parallaxSectionMid
-        
-        const parallaxLetters = document.querySelector('section.parallax-1 img')
-        
-        
-        
-}
-
-
-
-
+ 
+    const topSection = sectionsTag.offsetTop
+    const midSection = topSection + (sectionsTag.offsetHeight / 2)
+    const distanceToSection = midViewport - midSection
+    const parallaxTags = sectionsTag.querySelectorAll(`[data-parallax]`)
+    
+    //loop over each parallaxed tag
+    // this allows us to grab any element with the data attribute of 'data-parallax', and apply that speed to the element, rather than having to 				select individually based on classes-making it a much more flexible system
+    parallaxTags.forEach(tag =>{
+      const speed = parseFloat(tag.getAttribute('data-parallax'))
+      	tag.style.transform = `translate(0, ${distanceToSection * speed}px)`
+    })
+  })
 
 
 
